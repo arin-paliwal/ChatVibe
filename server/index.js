394362f0cc74b0b82ea45d946f63261d396f517f
Arin Paliwal <paliwalarin09@gmail.com>
2023-04-08 23:39:51 +0530

    Added Data

--- server/index.js
+++ server/index.js
@@ -13,7 +13,10 @@ import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js"
 import postsRoutes from "./routes/posts.js"
 import verifyToken  from './middleware/auth.js';
-import { createPost } from "./controllers/posts.js"
+import { createPost } from "./controllers/posts.js";
+import User from './models/User.js';
+import Post from './models/Post.js';
+import { users,posts } from './data/index.js';
 
 // CONFIGURATIONS
 const __filename=fileURLToPath(import.meta.url);
@@ -58,4 +61,6 @@ mongoose.connect(process.env.MONGO_URI,{
     useUnifiedTopology:true,
 }).then(()=>{
     app.listen(PORT,()=>console.log(`Server Started on PORT: ${PORT}`));
+    // User.insertMany(users);
+    // Post.insertMany(posts);
 }).catch((error)=> console.log(` ${error} did not connect`));
