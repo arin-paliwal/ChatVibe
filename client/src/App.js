16d0ae9b8f54af9e0c0fa02cd0a3da54d19baa69
Arin Paliwal <paliwalarin09@gmail.com>
2023-04-09 02:26:15 +0530

    Added Pages Structure 02:26 09-04

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homePage";
+import LoginPage from "scenes/loginPage";
+import profilePage from "scenes/profilePage";
 
 function App() {
   return (
     <div className="app">
-      
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage/>} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<profilePage />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
