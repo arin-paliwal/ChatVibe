7ecbd0c3587c7835382c856f7fe684d8a168d85c
Arin Paliwal <paliwalarin09@gmail.com>
2023-04-09 22:54:17 +0530

    Added Login Page

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
-
+import React from "react";
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from './Form.jsx'
 const LoginPage = () => {
+  const theme = useTheme();
+  const isNonMobileScreen = useMediaQuery("(min-width:1000px)");
   return (
-    <div>LoginPage</div>
-  )
-}
+    <Box>
+      <Box width="100px" backgroundColor={theme.palette.background.alt} 
+      p="1rem 6%"
+      textAlign="center"
+       >
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
+          ChatVibe
+        </Typography>
+      </Box>
+      <Box width={isNonMobileScreen?"50%":"93%"}
+      p="2rem"
+      m="2rem auto"
+      borderRadius="1.5rem"
+      backgroundColor={theme.palette.background.alt}
+      >
+        <Typography fontWeight="500" variant="h5" sx={{mb:"1.5rem"}}>
+          Welcome to ChatVibe, the Social Media Application for Vibing
+        </Typography>
+        <Form/>
+      </Box>
+    </Box>
+  );
+};
 
 export default LoginPage;
