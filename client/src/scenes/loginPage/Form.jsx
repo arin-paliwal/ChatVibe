5742879d8549dc517d06d25fc936368ca5eb73b7
Arin Paliwal <paliwalarin09@gmail.com>
2023-04-09 23:43:35 +0530

    .

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -1,12 +1,19 @@
 import { useState } from "react";
-import { Box,Button,TextField,Typography,useMediaQuery,useTheme  } from "@mui/material";
-import { EditOutlinedIcon } from "@mui/icons-material/EditOutlined"
-import {Formik} from "formik"
-import * as yup from "yup"
+import {
+  Box,
+  Button,
+  TextField,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+import { EditOutlinedIcon } from "@mui/icons-material/EditOutlined";
+import { Formik } from "formik";
+import * as yup from "yup";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { setLogin } from "state";
-import Dropzone from "react-dropzone"
+import Dropzone from "react-dropzone";
 import FlexBetween from "components/FlexBetween";
 const registerSchema = yup.object().shape({
   firstName: yup.string().required("required"),
@@ -30,46 +37,48 @@ const initialValuesRegister = {
   occupation: "",
   picture: "",
 };
-const initialValuesLogin={
-    email:"",
-    password:""
+const initialValuesLogin = {
+  email: "",
+  password: "",
 };
-const Form=()=>{
-    const [pageType,setPageType]=useState("login");
-    const {palette} = useTheme();
-    const dispatch=useDispatch();
-    const navigate=useNavigate();
-    const isNonMobileScreen=useMediaQuery("(min-width:600px)");
-    const isLogin=pageType==="login";
-    const isRegister=pageType==="register";
-    const handleFormSubmit=async(values,onSubmitProps)=>{};
-    return (
-        <Formik 
-        onSubmit={handleFormSubmit}
-        initialValues={isLogin?initialValuesLogin:initialValuesRegister}
-        validationSchema={isLogin?loginSchema:registerSchema}
-        >
-            {({
-                values,
-                errors,
-                touched,
-                handleBlur,
-                handleChange,
-                handleSubmit,
-                setFieldValue,
-                resetForm,
-            })=>(
-                <form onSubmit={handleSubmit}>
-                    <Box display="grid"
-                    gap="30px"
-                    gridTemplateColumns="repeat(4,minmax(0,1fr))"
-                    sx={{
-                        "& > div": {gridColumn:isNonMobileScreen?undefined:"span 4"},
-                    }}
-                    ></Box>
-                </form>
-            )
-            }
-        </Formik>
-    )
-}
+const Form = () => {
+  const [pageType, setPageType] = useState("login");
+  const { palette } = useTheme();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const isNonMobileScreen = useMediaQuery("(min-width:600px)");
+  const isLogin = pageType === "login";
+  const isRegister = pageType === "register";
+  const handleFormSubmit = async (values, onSubmitProps) => {};
+  return (
+    <Formik
+      onSubmit={handleFormSubmit}
+      initialValues={isLogin ? initialValuesLogin : initialValuesRegister}
+      validationSchema={isLogin ? loginSchema : registerSchema}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleBlur,
+        handleChange,
+        handleSubmit,
+        setFieldValue,
+        resetForm,
+      }) => (
+        <form onSubmit={handleSubmit}>
+          <Box
+            display="grid"
+            gap="30px"
+            gridTemplateColumns="repeat(4,minmax(0,1fr))"
+            sx={{
+              "& > div": {
+                gridColumn: isNonMobileScreen ? undefined : "span 4",
+              },
+            }}
+          ></Box>
+        </form>
+      )}
+    </Formik>
+  );
+};
