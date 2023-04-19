b587a74a2e4ad8e251de6ebd0cff69003595758d
Arin Paliwal <paliwalarin09@gmail.com>
2023-04-19 21:19:00 +0530

    Added Form

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -7,8 +7,8 @@ import {
   useMediaQuery,
   useTheme,
 } from "@mui/material";
-import { EditOutlinedIcon } from "@mui/icons-material/EditOutlined";
-import { Formik } from "formik";
+// import { EditOutlinedIcon } from "@mui/icons-material/EditOutlined";
+import { Formik, getIn } from "formik";
 import * as yup from "yup";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
@@ -75,6 +75,7 @@ const Form = () => {
               "& > div": {
                 gridColumn: isNonMobileScreen ? undefined : "span 4",
               },
+
             }}
           >
             {isRegister && (
@@ -83,6 +84,7 @@ const Form = () => {
                   label="First Name"
                   onBlur={handleBlur}
                   onChange={handleChange}
+                  value={values.firstName}
                   name="firstName"
                   error={
                     Boolean(touched.firstName) && Boolean(errors.firstName)
@@ -94,6 +96,7 @@ const Form = () => {
                   label="Last Name"
                   onBlur={handleBlur}
                   onChange={handleChange}
+                  value={values.lastName}
                   name="lastName"
                   error={Boolean(touched.lastName) && Boolean(errors.lastName)}
                   helperText={touched.lastName && errors.lastName}
@@ -103,6 +106,7 @@ const Form = () => {
                   label="Location"
                   onBlur={handleBlur}
                   onChange={handleChange}
+                  value={values.location}
                   name="location"
                   error={Boolean(touched.location) && Boolean(errors.location)}
                   helperText={touched.location && errors.location}
@@ -112,6 +116,7 @@ const Form = () => {
                   label="Occupation"
                   onBlur={handleBlur}
                   onChange={handleChange}
+                  value={values.occupation}
                   name="occupation"
                   error={
                     Boolean(touched.occupation) && Boolean(errors.occupation)
@@ -119,24 +124,101 @@ const Form = () => {
                   helperText={touched.occupation && errors.occupation}
                   sx={{ gridColumn: "span 4" }}
                 />
-                <Box gridColumn="span 4"
-                border={`1 px solid ${palette.neutral.medium}`}
-                borderRadius="5px"
-                p="1 rem"
+                <Box
+                  gridColumn="span 4"
+                  border={`1 px solid ${palette.neutral.medium}`}
+                  borderRadius="5px"
+                  p="1 rem"
                 >
-                    <Dropzone
+                  <Dropzone
                     acceptedFiles=".jpg, .jpeg, .png"
                     multiple={false}
-                    onDrop={(acceptedFiles)=>
-                    setFieldValue("picture")
+                    onDrop={(acceptedFiles) =>
+                      setFieldValue("picture", acceptedFiles[0])
                     }
-                    ></Dropzone>
+                  >
+                    {({ getRootProps, getInputProps }) => (
+                      <Box
+                        {...getRootProps()}
+                        border={`2px dashed ${palette.primary.main}`}
+                        p="1 rem"
+                        sx={{ "&:hover": { cursor: "pointer" } }}
+                      >
+                        <input {...getInputProps()} />
+                        {!values.picture ? (
+                          <p>Add image here !</p>
+                        ) : (
+                          <FlexBetween>
+                            <Typography>{values.picture.main}</Typography>
+                          </FlexBetween>
+                        )}
+                      </Box>
+                    )}
+                  </Dropzone>
                 </Box>
               </>
             )}
+            <TextField
+              label="Email"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.email}
+              name="email"
+              error={Boolean(touched.email) && Boolean(errors.email)}
+              helperText={touched.email && errors.email}
+              sx={{ gridColumn: "span 4" }}
+            />
+            <TextField
+              label="Password"
+              type="password"
+              onBlur={handleBlur}
+              onChange={handleChange}
+              value={values.password}
+              name="password"
+              error={Boolean(touched.password) && Boolean(errors.password)}
+              helperText={touched.password && errors.password}
+              sx={{ gridColumn: "span 4" }}
+            />
           </Box>
+
+
+          {/* BUTTONS */}
+          <Box>
+            <Button
+            fullWidth
+            type="submit"
+            sx={{
+              m:"2rem 0",
+              p:"1rem",
+              backgroundColor:palette.primary.main,
+              color:palette.primary.alt,
+              "&:hover":{
+                color:palette.primary.main
+              }
+            }}
+            >
+              {isLogin?"LOGIN":"REGISTER"}
+            </Button>
+            <Typography
+            onClick={()=>{
+              setPageType(isLogin?"register":"login");
+            }}
+            sx={{
+              textDecoration:"underline",
+              color:"palette.primary.light",
+              "&:hover":{
+                cursor:"pointer",
+                color:palette.primary.light,
+              },
+            }}
+            >
+              {isLogin?"Dont't have a account ! Sign up here":"Already have a account ! Login Here "}
+            </Typography>
+          </Box>
+
         </form>
       )}
     </Formik>
   );
 };
+export default Form
