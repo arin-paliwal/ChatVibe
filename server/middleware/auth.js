a908f0a6c3f1f186b660b4cd8d8a7055eeb3cde3
Arin Paliwal <paliwalarin09@gmail.com>
2023-04-21 23:34:53 +0530

    Added auth

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,18 +1,18 @@
 import jwt from "jsonwebtoken";
-const verifyToken=async(req,res,next)=>{
+const verifyToken = async (req, res, next) => {
     try {
-        let token=req.header("Authorization");
-        if(!token){
+        let token = req.header("Authorization");
+        if (!token) {
             return res.status(403).send("Acess Denied");
         }
-        if(token.startsWith("Bearer")){
-            token=token.slice(7,token.length).trimLeft();
+        if (token.startsWith("Bearer")) {
+            token = token.slice(7, token.length).trimLeft();
         }
-        const verified=jwt.verify(token,process.env.JWT_SECRET);
-        req.user=verified;
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = verified;
         next();
     } catch (err) {
-        res.status(500).json({error:err.message});
+        res.status(500).json({ error: err.message });
     }
 }
 export default verifyToken;
